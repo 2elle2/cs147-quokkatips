59a13acd70ac064670b7cbf8485b883f2ba7a5ce
Katherine <katherinechen139@gmail.com>
2022-02-25 17:46:59 -0800

    User can navigate between 3 tabs

---
+++ screens/MyGuidesScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function MyGuidesScreen() {
+    return (
+        <View>
+            <Text>This is the MyGuidesScreen</Text>
+        </View>
+    )
+}
