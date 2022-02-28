4ed8d52f8f15f7f8d81ec3e4ab1f40e422161b03
Esteban-Wu <estewu@stanford.edu>
2022-02-27 18:38:43 -0800

    draft branch re-base

--- App.js
+++ App.js
@@ -18,39 +18,14 @@ import ExploreScreen from "./screens/ExploreScreen";
 
 import Colors from "./Themes/colors";
 
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
 const Stack = createStackNavigator();
-
-const auth = getAuth();
-let uid = null;
-// Global event listener
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    uid = user.uid;
-    // ...
-  } else {
-    uid = null;
-    // User is signed out
-    // ...
-  }
-});
 
 export default function App() {
   const [user, setUser] = useState({}); // Use state to pass user object between components
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SignIn">
-          {props => <SignInScreen {...props} setUser={setUser} />}
-        </ Stack.Screen>
-        <Stack.Screen name="Home" options={{ headerShown: false }}>
-          {props => <HomeScreen {...props} user={user} />}
-        </Stack.Screen>
-
-        <Stack.Screen 
+        <Stack.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{ headerShown: false }}
@@ -87,11 +62,9 @@ export default function App() {
             ),
           }}
         /> */}
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Home" options={{ headerShown: false }}>
+          {props => <HomeScreen {...props} user={user} />}
+        </Stack.Screen>
         <Stack.Screen
           name="AppDetails"
           component={AppDetails}
